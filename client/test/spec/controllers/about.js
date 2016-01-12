71041a774935f35d70e2a4a550453cdfb33f8ecb
Mateusz Wilk <matewilk@gmail.com>
2016-01-12 04:12:25 +0000

    Karma config

--- client/test/spec/controllers/about.js
+++ client/test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('clientApp'));
+  beforeEach(module('M2App'));
 
   var AboutCtrl,
     scope;
